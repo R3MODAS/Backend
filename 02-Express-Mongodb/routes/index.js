df20510d1aede76db0975f5d190e026c8585355c
Sharadindu Das <remodas7774@gmail.com>
2024-02-02 16:26:14 +0530

    mongodb basics and basics of cookie and session

--- 02-Express-Mongodb/routes/index.js
+++ 02-Express-Mongodb/routes/index.js
@@ -2,26 +2,95 @@ var express = require('express');
 var router = express.Router();
 const User = require("./users")
 
-router.get('/', function (req, res) {
-  res.render('index', { title: 'Express' });
-});
+//! CRUD operations in mongodb
+// creating user
+// router.get("/create", async (req, res) => {
+//   const createdUser = await User.create({
+//     username: "remo",
+//     name: "Remo",
+//     age: 24
+//   })
+//   res.send(createdUser)
+// })
 
-// CRUD operations in mongodb
+// getting all users
+// router.get("/allusers", async (req, res) => {
+//   const allUsers = await User.find();
+//   if(allUsers.length != 0){
+//     res.send(allUsers)
+//   }
+//   else{
+//     res.status(400).json({
+//       message: 'There are no data for users !!!'
+//     })
+//   }
+// })
 
-router.get("/create", async (req, res) => {
-  const createdUser = await User.create({
-    username: "_remo_",
-    name: "Remo",
-    age: 24
-  })
-  res.send(createdUser)
-})
+// finding single user 
+// router.get("/finduser", async (req,res) => {
+//   const findUser = await User.findOne({username: "ram"})
+//   if(findUser){
+//     res.send(findUser)
+//   }
+//   else{
+//     res.status(400).json({
+//       message: 'User not found !!!'
+//     })
+//   }
+// })
 
-router.get("/allusers", async (req, res) => {
-  const allUsers = await User.find();
-  res.send(allUsers)
-})
+// deleting user
+// router.get("/delete", async(req,res) => {
+//   const deletedUser = await User.findOneAndDelete({
+//     name: "Remo"
+//   })
+//   if(deletedUser){
+//     res.send(deletedUser);
+//   }
+//   else{
+//     res.status(400).json({
+//       message: 'User not found !!!'
+//     })
+//   }
+// })
 
-router.get("/")
+//! Session
+// router.get('/', (req, res) => {
+//   req.session.ban = true;
+//   res.render('index', { title: 'Express' });
+// });
+
+// router.get("/checkban", (req,res) => {
+//   console.log(req.session)
+//   if(req.session.ban === true){
+//     res.send("You are banned")
+//   }else{
+//     res.send("You are not banned")
+//   }
+// })
+
+// router.get("/removeban", (req,res) => {
+//   req.session.destroy((err) => {
+//     if(err) throw err
+//     res.send("Ban has been removed")
+//   });
+
+// })
+
+//! Cookie
+// router.get('/', (req, res) => {
+//   res.cookie("age",24)
+//   res.render('index', { title: 'Express' });
+// });
+
+// router.get("/read", (req,res) => {
+//   console.log(req.cookies.age)
+//   res.send("Read the cookie")
+// })
+
+// router.get("/delete", (req,res) => {
+//   res.clearCookie("age")
+//   res.send("Delete the cookie")
+// })
 
 module.exports = router;
--- Readme.md
+++ Readme.md
@@ -26,7 +26,29 @@ Code Side and MongoDB Side
 ## CRUD operations in MongoDB
 - ModelWeCreated -> UserModel and operations related to DB are mostly asynchronous so use async-await
 
-- Create Data (create a user)  Eg: UserModel.create({ // include the fields with the value name: "Ram", age : 99 })
+- Create Data (create a user) using UserModel.create({ name: "Ram", age : 99 })
 
-- Read Data (finds all the users) Eg:  UserModel.find()
-  
+- Read Data (finds all the users) using UserModel.find() and it returns a blank array if no users are found it returns null otherwise returns the array of user
+
+- Read Data (find one of the user) using UserModel.findOne({username: "Remo"}) is used to find one element out of loads of users and returns null if no user is found then it returns null otherwise returns the user
+
+- Delete Data (delete a user) using UserModel.findOneAndDelete({username: "Remo"}) is used to find one element and delete the data and if no data is found then it returns null otherwise returns the user
+
+## Cookie and Session
+- Data saved on browser/client -> Cookie and Data saved on server -> Session
+
+- Session data is more safe than Cookie data as it can't be easily hacked by anyone
+
+- npm i express-session to install session package and we can use it now to use session data
+
+To set the middleware session we can use it like this
+
+- app.use(session({
+    resave: false // if the session value is not changed then don't resave the value to avoid load on server
+    saveUninitialized: false // the data has no name then don't save it to avoid too much unnecessary data and cause load on server
+    secret: secret_key // secret_key which will be used to encrypt the data on server
+}))
+
+- req.session.name = value to create a session, req.session.name to read the session from the server, req.session.destroy to remove the session from the server
+
+- res.cookie(name,value) to create a cookie and sending it to the browser, req.cookies.name to read the cookie from the browser, res.clearCookie(name) to delete the cookie from the browser
