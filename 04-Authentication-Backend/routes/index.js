012663ccadd3764733fa338043c4abcc74717018
Sharadindu Das <remodas7774@gmail.com>
2024-02-06 16:03:38 +0530

    authentication authorization done

--- 04-Authentication-Backend/routes/index.js
+++ 04-Authentication-Backend/routes/index.js
@@ -6,4 +6,47 @@ const localStrategy = require("passport-local")
 
 passport.use(new localStrategy(User.authenticate()))
 
+router.get("/", (req,res) => {
+    res.render("index")
+})
+
+router.get("/profile", isLoggedIn , (req, res) => {
+    res.render("profile")
+})
+
+router.post("/register", (req, res) => {
+    const userdata = new User({
+        username: req.body.username,
+        secret: req.body.secret
+    })
+
+    User.register(userdata, req.body.password)
+        .then((registeredUser) => {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/profile");
+            })
+        })
+})
+
+router.post("/login", passport.authenticate("local", {
+    successRedirect: "/profile",
+    failureRedirect: "/login"
+}), (req, res) => {
+
+})
+
+router.get("/logout", (req,res,next) => {
+    req.logout((err) => {
+        if(err) return next(err)
+        res.redirect("/")
+    })
+})
+
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next()
+    }
+    res.redirect("/")
+}
+
 module.exports = router;
--- 04-Authentication-Backend/routes/users.js
+++ 04-Authentication-Backend/routes/users.js
@@ -1,13 +1,15 @@
 const mongoose = require("mongoose")
+const plm = require("passport-local-mongoose")
 
 // connection of mongodb server and adding a db
 mongoose.connect("mongodb+srv://remo:remo123@cluster0.zi35bim.mongodb.net/PracticeDB")
 
 const userSchema = mongoose.Schema({
   username: String,
-  nickname: String,
-  description: String,
-  categories: Array,
-}, {timestamps: true})
+  password: String,
+  secret: String
+})
+
+userSchema.plugin(plm)
 
 module.exports = mongoose.model("User", userSchema)
